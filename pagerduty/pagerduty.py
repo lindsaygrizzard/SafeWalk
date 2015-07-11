26937589bf46f693d4815e151ca6f6ade7c54964
Natalie <taliegarcia@gmail.com>
2015-07-11 12:50:54 -0700

    pagerduty.py receiving 200 ok response from api

--- pagerduty/pagerduty.py
+++ pagerduty/pagerduty.py
@@ -3,14 +3,17 @@ import os
 import json
 
 pg_token=os.environ['PAGERDUTY_TOKEN']
-headers = {'Accept': 'application/json'} # returns json
+headers = {'Accept': 'application/json', 
+        'Authorization' : 'Token token=%s' % pg_token}
+        
 print pg_token
 
 test_url = "https://apitester.pagerduty.com/api/v1/users"
 
 response = requests.get(test_url, 
-                        headers=headers,
-                        verify = True)
+                        headers=headers
+                        )
+                        # verify=True)
 
     # #Here is the info one can get about users, info can also be inserted
     # #alerts can also be found and inserted

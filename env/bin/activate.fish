5979f8224ee15676572fc2365fecdf5ec33d6492
Natalie <taliegarcia@gmail.com>
2015-07-11 11:26:51 -0700

    created virtualenv taliegarcia

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lindsaygrizzard/Desktop/safe_walk/env"
+set -gx VIRTUAL_ENV "/Users/Natalie/Documents/hackbright-src/SafeWalk/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

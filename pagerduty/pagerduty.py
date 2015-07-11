ddbc5e5c8e4f0fc5213b787fc8e8f715fb71184f
Natalie <taliegarcia@gmail.com>
2015-07-11 12:44:58 -0700

    updated requirements.txt

--- pagerduty/pagerduty.py
+++ pagerduty/pagerduty.py
@@ -3,15 +3,14 @@ import os
 import json
 
 pg_token=os.environ['PAGERDUTY_TOKEN']
+headers = {'Accept': 'application/json'} # returns json
 print pg_token
 
-    # response = requests.get(
-    # "https://apitester.pagerduty.com/api/v1/users",
-    # headers = {
-    #     "Authorization": pg_token,
-    # },
-    # verify = True,  # Verify SSL certificate
-    # )
+test_url = "https://apitester.pagerduty.com/api/v1/users"
+
+response = requests.get(test_url, 
+                        headers=headers,
+                        verify = True)
 
     # #Here is the info one can get about users, info can also be inserted
     # #alerts can also be found and inserted

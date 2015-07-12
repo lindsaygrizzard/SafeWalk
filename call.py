4954af7456a7be57fc14318e0daad9f28705c86a
Natalie <taliegarcia@gmail.com>
2015-07-11 21:02:43 -0700

    revised send_sms function to take 3 parameters: caller, receive, message in call.py

--- call.py
+++ call.py
@@ -5,15 +5,12 @@ import os
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
-default_caller=os.environ['LG']
-default_receiver=os.environ['AP']
 
-
-def send_sms(caller, receiver):
+def send_sms(caller, receiver, message):
     try:
         client = twilio.rest.TwilioRestClient(account_sid, auth_token)
         message = client.messages.create(
-            body="Hello World",
+            body=message,
             to=receiver,
             from_=caller
         )
@@ -21,4 +18,7 @@ def send_sms(caller, receiver):
         print e
 
 if __name__ == "__main__":
-    send_sms(default_caller, default_receiver)
+    default_caller=os.environ['LG']
+    default_receiver=os.environ['AP']
+    default_message="I will walk with you on emPOWERwalk."
+    send_sms(default_caller, default_receiver, default_message)
